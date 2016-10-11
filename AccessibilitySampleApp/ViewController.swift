01e035f48f81def15c765c111f6bd8cfac1effcc
Abhay Curam <abhay.curam@workday.com>
2016-10-11 12:52:00 -0700

    Initial Commit

---
+++ AccessibilitySampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AccessibilitySampleApp
+//
+//  Created by Abhay Curam on 10/11/16.
+//  Copyright © 2016 Abhay Curam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
